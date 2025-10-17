8df9184c4fa28274dc970ab9c0736942e58e7bd4
Dimitrios Dimitrakopoulos <123244492+ddimitrakopoulos@users.noreply.github.com>
2025-10-17 16:33:47 +0300

    Add files via upload

---
+++ api/api-function1/Function1/index.js
@@ -0,0 +1,12 @@
+module.exports = async function (context, req) {
+  const randomNum = Math.floor(Math.random() * 100);
+
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: {
+      message: "Hello from Function 1!",
+      randomNum
+    }
+  };
+};
