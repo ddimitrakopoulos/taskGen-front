db410fb7347a2c7051cba65e71e618659cd99056
Dimitrios Dimitrakopoulos <123244492+ddimitrakopoulos@users.noreply.github.com>
2025-10-17 12:34:48 +0300

    Delete api-function1 directory

--- api-function1/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function (context, req) {
-  const randomNum = Math.floor(Math.random() * 1000);
-  context.res = {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: { message: "Hello from Function 1!", randomNum }
-  };
-};
