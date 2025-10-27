c7df35ce6342d57e83922cdf677e0f2942212742
Dimitrios Dimitrakopoulos <ddimitrakopoulos27@gmail.com>
2025-10-27 14:57:35 +0200

    Revert "Create startup.sh"
    This reverts commit 49fb911a2a36b82cccdb372ecfe89d65a3402c1e.

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# unzip node_modules if not already there
-if [ ! -d "node_modules" ] && [ -f "node_modules.zip" ]; then
-  unzip node_modules.zip -d .
-fi
-
-# start the server
-node server.js
