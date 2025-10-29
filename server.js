ae54495ea86604a6ecb468d8a2ad82b5d1d8df23
Dimitrios Dimitrakopoulos <123244492+ddimitrakopoulos@users.noreply.github.com>
2025-10-29 14:02:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,9 +21,3 @@ app.use("/api/tasks", api_table);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-
